00a27d6d35ce1c5feb8918999b43c48192b5b3a8
Andrew Owino <andystudy4@gmail.com>
2024-04-19 17:16:13 +0300

    Refactor

--- src/routes/expensesRouter.mjs
+++ src/routes/expensesRouter.mjs
@@ -6,33 +6,35 @@ import { checkSchema, matchedData, validationResult } from "express-validator";
 const router = Router();
 
 router.get('/api/expenses', async (req, res) => {
-    await Expense.find()
-        .then((foundExpenses) => {
-            console.log("Fetching expenses");
-            return res.status(200).send(foundExpenses);
-        })
-        .catch((error) => {
-            console.error(`Error fetching expenses: ${error}`);
-            return res.sendStatus(500);
-        });
+    try {
+        const foundExpenses = await Expense.find();
+        console.log("Fetching expenses");
+        return res.status(200).send(foundExpenses);
+    } catch (error) {
+        console.error('Error fetching expenses:', error);
+        return res.sendStatus(500);
+    }
 });
 
 
 router.post('/api/expenses', checkSchema(createExpenseValidationSchema), async (req, res) => {
     const errorResult = validationResult(req);
 
-    if (!errorResult.isEmpty()) return res.status(400).send(errorResult);
-
-    const data = matchedData(req)
-    const newExpense = new Expense(data);
+    if (!errorResult.isEmpty()) {
+        return res.status(400).send(errorResult);
+    }
 
     try {
+        const data = matchedData(req);
+        const newExpense = new Expense(data);
         const savedExpense = await newExpense.save();
+        console.log("Saved new expense");
         return res.status(201).send(savedExpense);
-    } catch (err) {
-        console.log(err);
-        return res.sendStatus(400);
+    } catch (error) {
+        console.error('Error saving expense:', error);
+        return res.sendStatus(500);
     }
 });
+
 
 export default router;
