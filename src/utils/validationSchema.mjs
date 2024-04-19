a106fede196142e602860a51fa533affc35c9091
Andrew Owino <andystudy4@gmail.com>
2024-04-20 00:51:40 +0300

    Improve validation of Expense creation

--- src/utils/validationSchema.mjs
+++ src/utils/validationSchema.mjs
@@ -1,22 +1,51 @@
 export const createExpenseValidationSchema = {
     date: {
         notEmpty: {
-            errorMessage: "Date cannot not be empty"
+            errorMessage: "Date cannot be empty"
         },
+        isISO8601: {
+            errorMessage: "Date must be in ISO 8601 format"
+        }
     },
     name: {
         notEmpty: {
-            errorMessage: "Expense name cannot not be empty"
+            errorMessage: "Expense name cannot be empty"
         },
+        isLength: {
+            options: { min: 1, max: 255 },
+            errorMessage: "Expense name must be between 1 and 255 characters"
+        }
     },
     amount: {
         notEmpty: {
-            errorMessage: "Amount cannot not be empty"
+            errorMessage: "Amount cannot be empty"
         },
+        isNumeric: {
+            errorMessage: "Amount must be a number"
+        },
+        isFloat: {
+            options: { min: 0 },
+            errorMessage: "Amount must be a positive number"
+        }
     },
     category: {
         notEmpty: {
-            errorMessage: "Category cannot not be empty"
+            errorMessage: "Category cannot be empty"
         },
+        isIn: {
+            options: [['Groceries', 'Utilities', 'Rent', 'Transportation', 'Entertainment', 'Healthcare', 'Insurance', 'Dining Out', 'Clothing', 'Miscellaneous']],
+            errorMessage: "Invalid category"
+        }
+    }
+};
+
+export const createUserValidationSchema = {
+    username: {
+        notEmpty: {
+            errorMessage: "Username cannot be empty",
+        }
     },
-}
+    password: {
+        notEmpty: true,
+    },
+};
