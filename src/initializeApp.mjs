55097cb69adfbdd148ed68999ac1ff5c8b56232e
Andrew Owino <andystudy4@gmail.com>
2024-04-20 01:40:37 +0300

    Access control

--- src/initializeApp.mjs
+++ src/initializeApp.mjs
@@ -11,7 +11,9 @@ export function initializeApp() {
     const app = express();
 
     app.use(express.json());
-    app.use(cors())
+    app.use(cors({
+        origin: process.env.FRONTEND_URL,
+    }))
 
     app.use(routes);
 
